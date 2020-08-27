938edc7e87fd98674a686395e320ac9911137803
Nicholas J Morris <nicholasjmorris93@gmail.com>
2020-08-27 13:17:12 -0400

    Add files via upload

---
+++ house_plots.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+https://plotly.com/python/plotly-express/
+
+@author: Nick
+"""
+
+import pandas as pd
+import plotly.express as px
+from plotly.offline import plot
+
+df = pd.read_csv("house.csv").drop(columns="Id")
+
+# define the target feature and other meaningful features
+targets = ["SalePrice"]
+features = ["OverallQual", "YearBuilt", "GrLivArea", "GarageCars", "GarageArea", 
+            "TotalBsmtSF", "PavedDrive", "BldgType"]
+
+# plot a matrix of scatter plots to see the whole data set
+fig = px.scatter_matrix(df, 
+                        dimensions=["SalePrice", "OverallQual", "YearBuilt", 
+                                    "GrLivArea", "GarageCars", "GarageArea", 
+                                    "TotalBsmtSF"],
+                        color="PavedDrive",
+                        opacity=0.7)
+fig.update_traces(diagonal_visible=False)
+plot(fig)
+
+# these 3 variables have the strongest linear relationship with eachother
+group = ["SalePrice", "GrLivArea", "TotalBsmtSF"]
+
+# plot the group of 3 variables across categories
+fig = px.scatter_3d(df, x="SalePrice", y="GrLivArea", z="TotalBsmtSF",
+                    color="PavedDrive", opacity=0.7)
+plot(fig)
+
+# plot two variables across categories
+fig = px.density_contour(df, x="SalePrice", y="TotalBsmtSF", marginal_x="histogram", 
+                         marginal_y="histogram", color="PavedDrive")
+plot(fig)
+
+# plot SalePrice across categories
+fig = px.strip(df, y="SalePrice", color="BldgType")
+plot(fig)
